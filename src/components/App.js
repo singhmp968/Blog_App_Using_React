28c3e6aa9854dcd46e35a760b0bbea8397ce5647
codingninjasstudio <user@example.com>
2021-01-22 16:10:38 +0530

    Setting up routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
+import { Switch, Route } from 'react-router-dom';
+import { Navbar, Home, CreatePost, PostDetail } from './';
+
 function App() {
   return (
-    <div className="App">
-      Starting React Blog
+    <div className="container">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/post/:postId" component={PostDetail} />
+        <Route exact path="/create-post" component={CreatePost} />
+      </Switch>
     </div>
   );
 }
