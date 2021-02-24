a168b967198877aa4eaf2d02157c2e9a706b5911
codingninjasstudio <user@example.com>
2021-02-24 16:01:07 +0530

    adding radium

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import { Switch, Route } from 'react-router-dom';
+import { StyleRoot } from 'radium';
 import { Navbar, Home, CreatePost, PostDetail } from './';
 
 function App() {
   return (
-    <div className="container">
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/post/:postId" component={PostDetail} />
-        <Route exact path="/create-post" component={CreatePost} />
-      </Switch>
-    </div>
+    <StyleRoot>
+      <div className="container">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/post/:postId" component={PostDetail} />
+          <Route exact path="/create-post" component={CreatePost} />
+        </Switch>
+      </div>
+    </StyleRoot>
   );
 }
 
--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Radium from 'radium';
+
 import { firestore } from '../firebase';
 
 function PostDetail() {
@@ -19,10 +21,30 @@ function PostDetail() {
 
   return (
     <div className="post-detail">
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
+      <h1 style={styles.heading}>{post.title}</h1>
+      <p style={styles.postDetail}>{post.content}</p>
     </div>
   );
 }
 
-export default PostDetail;
+export default Radium(PostDetail);
+
+const styles = {
+  postDetail: {
+    border: '1px solid #e1e1e1',
+    padding: 5,
+    paddingTop: 10,
+    borderRadius: 5,
+
+    '@media(max-width: 720px)': {
+      color: 'pink',
+    },
+  },
+  heading: {
+    textAlign: 'center',
+    margin: 0,
+    ':hover': {
+      color: 'red',
+    },
+  },
+};
