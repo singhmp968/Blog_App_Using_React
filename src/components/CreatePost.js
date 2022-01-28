6c3c133be3448cc27a1eb6202f7d95b73aabd8c4
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2022-01-28 21:16:30 +0530

    added ;custom hooks logic

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,3 +1,58 @@
+// using with custome componenets
+import { firestore } from '../firebase';
+import { useFormInput } from '../hooks';
+
+function CreatePost() {
+  // using custom hooks
+  const title = useFormInput('');
+  const subTitle = useFormInput('');
+  const content = useFormInput('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log('title', title);
+    console.log('subTitle', subTitle);
+    console.log('content', content);
+
+    firestore.collection('posts').add({
+      title: title.value, // getting from hooks i.e useFormInput value={title} onChange={(e) => setTitle(e.target.value)} as in hooks we have value and onChange
+      content: content.value,
+      subTitle: subTitle.value,
+      createdAt: new Date(),
+    });
+  }
+
+  return (
+    <div className="create-post">
+      <h1>Create Post</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label>Title</label>
+          {/* here ... will be save  */}
+          <input {...title} />
+        </div>
+
+        <div className="form-field">
+          <label>Sub Title</label>
+          <input {...subTitle} />
+        </div>
+
+        <div className="form-field">
+          <label>Content</label>
+          <textarea {...content}></textarea>
+        </div>
+
+        <button className="create-post-btn">Create Post</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreatePost;
+
+/* without custom hooks
 import { useState } from 'react';
 import { firestore } from '../firebase';
 
@@ -58,3 +113,4 @@ function CreatePost() {
 }
 
 export default CreatePost;
+*/
---
+++ src/hooks.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+// every custom hook should start wit  use
+export function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    onChange: handleChange,
+  };
+}
